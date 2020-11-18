63de67ec680821aa77752fbd1f1f1dddadd72aab
Kristy Li <kristyli20.0@gmail.com>
2020-11-18 14:27:08 -0500

    feat: Set up Express server and connect to MongoDB

---
+++ server/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
+
+const app = express();
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use((err, req, res) => {
+  console.error(err);
+  console.error(err.stack);
+
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+module.exports = app;
