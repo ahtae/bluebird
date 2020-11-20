14e9b2ad860aa383fb0a87a9a4a81506bd023bd1
Kristy Li <kristyli20.0@gmail.com>
2020-11-20 16:48:10 -0500

    feat: Create a User model

--- server/models/user.js
+++ server/models/user.js
@@ -1,28 +1,42 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  handle: {
-    type: String,
-    minlength: 6,
-    maxLength: 30,
-    required: true,
+const UserSchema = new mongoose.Schema(
+  {
+    handle: {
+      type: String,
+      minlength: 6,
+      maxLength: 30,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw0_oEK4oat8ib811DHDOM-v&ust=1605839857463000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjQveTJje0CFQAAAAAdAAAAABAD',
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      maxLength: 30,
+      required: true,
+    },
+    bio: String,
+    website: String,
+    location: String,
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+    ],
   },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now(),
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    minlength: 6,
-    maxLength: 30,
-    required: true,
-  },
-});
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+  }
+);
 
 UserSchema.set('toJSON', {
   transform: (_document, returnedObject) => {
