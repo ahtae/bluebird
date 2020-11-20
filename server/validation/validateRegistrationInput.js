4868747a17745c4f8a3d517bef3149e6ebb591ac
Kristy Li <kristyli20.0@gmail.com>
2020-11-20 16:53:49 -0500

    feat: Create a function to validate registration input

--- server/validation/validateRegistrationInput.js
+++ server/validation/validateRegistrationInput.js
@@ -13,6 +13,10 @@ const validateRegistrationInput = (data) => {
 
   if (Validator.isEmpty(data.handle)) {
     errors.handle = 'Handle field is required!';
+  } else {
+    if (data.handle.length < 6 || data.handle.length > 30) {
+      errors.handle = 'The handle must be between 6 and 30 in length!';
+    }
   }
 
   if (Validator.isEmpty(data.email)) {
@@ -28,12 +32,16 @@ const validateRegistrationInput = (data) => {
   }
 
   if (Validator.isEmpty(data.confirmPassword)) {
-    errors.firstName = 'Confirm password field is required!';
+    errors.confirmPassword = 'Confirm password field is required!';
   }
 
   if (data.confirmPassword !== data.password) {
-    errors.lastName =
+    errors.confirmPassword =
       'Password field and confirm password field must be the same!';
+  } else {
+    if (data.password.length < 6 || data.password.length > 30) {
+      errors.password = 'The password must be between 6 and 30 in length!';
+    }
   }
 
   return {
