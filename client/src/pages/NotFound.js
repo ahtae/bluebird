8e3bb4d09b2e2ce50a10f6937e1e67b02beb32cd
Kristy Li <kristyli20.0@gmail.com>
2020-11-21 18:51:24 -0500

    feat: Create a NotFound page

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = (theme) => ({
+  ...theme.notFound,
+});
+
+const NotFound = ({ classes }) => {
+  return (
+    <div>
+      <SentimentVeryDissatisfiedIcon color="primary" className={classes.icon} />
+      <Typography color="primary" variant="h3" className={classes.pageTitle}>
+        404
+      </Typography>
+      <Typography color="primary" variant="h5">
+        Page Not Found
+      </Typography>
+    </div>
+  );
+};
+
+export default withStyles(styles)(NotFound);
