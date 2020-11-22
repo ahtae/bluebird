956bd493b4e90a8e7d1cdf4b29bd34225e06b24b
Kristy Li <kristyli20.0@gmail.com>
2020-11-21 19:12:05 -0500

    fix: Use the redux store to determine whether or not the user has access to certain routes

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => {
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = useSelector((state) => state.user.authenticated);
+
   return (
     <Route
       {...rest}
