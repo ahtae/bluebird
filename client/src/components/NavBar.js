13c8e6aeb07955c3c83653d3903405e2da437b3b
Kristy Li <kristyli20.0@gmail.com>
2020-11-21 19:08:51 -0500

    fix: Render Nav bar buttons conditionally and to clear errors if the user goes to a different page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
 import birdLogo from '../assets/images/bird.png';
+import { useDispatch } from 'react-redux';
+import { clearErrors } from '../redux/actionCreators/ui';
+import { withLastLocation } from 'react-router-last-location';
+import { useSelector } from 'react-redux';
 
 const styles = (theme) => ({
   ...theme.navbar,
 });
 
-const NavBar = ({ classes }) => {
+const NavBar = ({ lastLocation, classes }) => {
+  const dispatch = useDispatch();
+  const authenticated = useSelector((state) => state.user.authenticated);
+
+  useEffect(() => {
+    dispatch(clearErrors());
+  }, [dispatch, lastLocation]);
+
   return (
     <AppBar>
       <Toolbar>
@@ -23,33 +34,39 @@ const NavBar = ({ classes }) => {
         >
           Home
         </Button>
-        <Button
-          color="inherit"
-          className={classes.button}
-          component={Link}
-          to="/login"
-        >
-          Login
-        </Button>
-        <Button
-          color="inherit"
-          className={classes.button}
-          component={Link}
-          to="/signup"
-        >
-          Sign up
-        </Button>
-        <Button
-          color="inherit"
-          className={classes.button}
-          component={Link}
-          to="/dashboard"
-        >
-          Dashboard
-        </Button>
+        {!authenticated && (
+          <React.Fragment>
+            <Button
+              color="inherit"
+              className={classes.button}
+              component={Link}
+              to="/login"
+            >
+              Login
+            </Button>
+            <Button
+              color="inherit"
+              className={classes.button}
+              component={Link}
+              to="/signup"
+            >
+              Sign up
+            </Button>{' '}
+          </React.Fragment>
+        )}
+        {authenticated && (
+          <Button
+            color="inherit"
+            className={classes.button}
+            component={Link}
+            to="/dashboard"
+          >
+            Dashboard
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
 };
 
-export default withStyles(styles)(NavBar);
+export default withLastLocation(withStyles(styles)(NavBar));
