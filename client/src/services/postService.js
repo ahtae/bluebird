1674bf4e3b60e4f639473a102f24074021c7fe20
Kristy Li <kristyli20.0@gmail.com>
2020-11-23 12:30:40 -0500

    feat: Add likePost and unlikePost functions in postService

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -14,6 +14,32 @@ const createPost = async (post, credentials) => {
   return response;
 };
 
-const postService = { getPosts, createPost };
+const likePost = async (postId, credentials) => {
+  const response = await axios.post(
+    `${baseUrl}/${postId}/like`,
+    {},
+    credentials
+  );
+
+  return response;
+};
+
+const unlikePost = async (postId, credentials) => {
+  const response = await axios.post(
+    `${baseUrl}/${postId}/unlike`,
+    {},
+    credentials
+  );
+
+  return response;
+};
+
+const deletePost = async (postId, credentials) => {
+  const response = await axios.delete(`${baseUrl}/${postId}`, credentials);
+
+  return response;
+};
+
+const postService = { getPosts, createPost, likePost, unlikePost, deletePost };
 
 export default postService;
