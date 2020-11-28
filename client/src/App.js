f4b91c4a6b24d01caca42f9218a1e09d9461e8d8
Kristy Li <kristyli20.0@gmail.com>
2020-11-28 13:59:30 -0500

    feat: Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Dashboard from './pages/Dashboard';
-import NavBar from './components/NavBar';
-import NotFound from './pages/NotFound';
+import React from 'react';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { LastLocationProvider } from 'react-router-last-location';
-import MuiTheme from './utils/theme';
-import PrivateRoute from './components/PrivateRoute';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import UserProfile from './pages/UserProfile';
 import { ToastProvider } from 'react-toast-notifications';
+import { Provider } from 'react-redux';
+import {
+  Dashboard,
+  Home,
+  Login,
+  NotFound,
+  Notifications,
+  SignUp,
+  SinglePost,
+  UserProfile,
+} from '../src/pages';
+import { NavBar, PrivateRoute } from './components';
+import MuiTheme from './utils/theme';
+import store from './redux/store';
 import './App.css';
-import React from 'react';
 
 const theme = createMuiTheme(MuiTheme);
 
@@ -34,12 +37,22 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/signup" component={SignUp} />
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
                 <PrivateRoute
                   exact
-                  path="/user/:userHandle"
+                  path="/users/:userHandle"
                   component={UserProfile}
+                />
+                <PrivateRoute
+                  exact
+                  path="/posts/:postId"
+                  component={SinglePost}
+                />
+                <PrivateRoute
+                  exact
+                  path="/notifications"
+                  component={Notifications}
                 />
                 <Route component={NotFound} />
               </Switch>
