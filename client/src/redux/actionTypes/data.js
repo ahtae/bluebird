5e28c6962cf2d42fc67ff36a09c9d441044d9296
Kristy Li <kristyli20.0@gmail.com>
2020-11-28 20:21:16 -0500

    feat: Create data related action types

---
+++ client/src/redux/actionTypes/data.js
@@ -0,0 +1,13 @@
+export const GET_USER = 'GET_USER';
+export const FOLLOW_USER = 'FOLLOW_USER';
+export const UNFOLLOW_USER = 'UNFOLLOW_USER';
+export const GET_POSTS = 'GET_POSTS';
+export const GET_POST = 'GET_POST';
+export const CREATE_POST = 'CREATE_POST';
+export const LIKE_POST = 'LIKE_POST';
+export const UNLIKE_POST = 'UNLIKE_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const CREATE_COMMENT = 'CREATE_COMMENT';
+export const LIKE_COMMENT = 'LIKE_COMMENT';
+export const UNLIKE_COMMENT = 'UNLIKE_COMMENT';
+export const DELETE_COMMENT = 'DELETE_COMMENT';
