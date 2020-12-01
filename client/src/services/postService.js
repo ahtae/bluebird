01c72b2455604d1c475f7fdb97e60699c4ee7d4c
Kristy Li <kristyli20.0@gmail.com>
2020-11-30 22:56:23 -0500

    feat: Create postService functions

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -34,12 +34,68 @@ const unlikePost = async (postId, credentials) => {
   return response;
 };
 
+const likeComment = async (postId, commentId, credentials) => {
+  const response = await axios.post(
+    `${baseUrl}/${postId}/comments/${commentId}/like`,
+    {},
+    credentials
+  );
+
+  return response;
+};
+
+const unlikeComment = async (postId, commentId, credentials) => {
+  const response = await axios.post(
+    `${baseUrl}/${postId}/comments/${commentId}/unlike`,
+    {},
+    credentials
+  );
+
+  return response;
+};
+
 const deletePost = async (postId, credentials) => {
   const response = await axios.delete(`${baseUrl}/${postId}`, credentials);
 
   return response;
 };
 
-const postService = { getPosts, createPost, likePost, unlikePost, deletePost };
+const getPost = async (postId, credentials) => {
+  const response = await axios.get(`${baseUrl}/${postId}`, credentials);
+
+  return response;
+};
+
+const createComment = async (postId, comment, credentials) => {
+  const response = await axios.post(
+    `${baseUrl}/${postId}/comments`,
+    comment,
+    credentials
+  );
+
+  return response;
+};
+
+const deleteComment = async (postId, commentId, credentials) => {
+  const response = await axios.delete(
+    `${baseUrl}/${postId}/comments/${commentId}`,
+    credentials
+  );
+
+  return response;
+};
+
+const postService = {
+  deleteComment,
+  createComment,
+  getPost,
+  getPosts,
+  createPost,
+  likePost,
+  unlikePost,
+  likeComment,
+  unlikeComment,
+  deletePost,
+};
 
 export default postService;
