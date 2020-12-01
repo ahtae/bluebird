709953d9d1b561c61b9eb12aea456ac9882097f5
Kristy Li <kristyli20.0@gmail.com>
2020-11-30 23:30:03 -0500

    refactor: Import and export all the models

---
+++ server/models/index.js
@@ -0,0 +1,13 @@
+const Comment = require('./comment');
+const Follower = require('./follower');
+const Notification = require('./notification');
+const Post = require('./post');
+const User = require('./user');
+
+module.exports = {
+  Comment,
+  Follower,
+  Notification,
+  Post,
+  User,
+};
