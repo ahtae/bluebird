8e2a09973fd9cc8718580b132412bfafafe9caca
Kristy Li <kristyli20.0@gmail.com>
2020-11-30 23:17:27 -0500

    feat: Create various routes for /api/user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const User = require('../models/user');
-const getTokenFrom = require('../utils/getTokenFrom');
 const jwt = require('jsonwebtoken');
+const getTokenFrom = require('../utils/getTokenFrom');
 const config = require('../utils/config');
 const upload = require('../utils/multerConfig');
 const cloudinary = require('../utils/cloudinaryConfig');
 const cleanUserDetails = require('../utils/cleanUserDetails');
+const validateProfile = require('../../server/validation/validateProfile');
+const { User, Post, Comment, Follower } = require('../models');
 
 const userRouter = express.Router();
 
@@ -20,7 +21,16 @@ userRouter.get('/:userHandle', getTokenFrom, async (req, res) => {
       const user = await User.findOne({ handle: userHandle });
 
       if (user) {
-        await user.populate('posts').sort('-createdAt').execPopulate();
+        await user
+          .populate('posts')
+          .populate({
+            path: 'followers',
+            populate: {
+              path: 'user',
+              model: 'User',
+            },
+          })
+          .execPopulate();
 
         res.status(200).json(user);
       } else {
@@ -34,8 +44,8 @@ userRouter.get('/:userHandle', getTokenFrom, async (req, res) => {
 
 userRouter.post(
   '/image',
-  getTokenFrom,
   upload.single('image'),
+  getTokenFrom,
   async (req, res) => {
     try {
       const decodedToken = jwt.verify(req.token, config.JWT_SECRET);
@@ -45,13 +55,43 @@ userRouter.post(
           error: 'Unauthorized!',
         });
       } else {
-        const user = await User.findById(decodedToken.id);
+        const user = await User.findById(decodedToken.id)
+          .populate('posts')
+          .populate({
+            path: 'followers',
+            populate: {
+              path: 'user',
+              model: 'User',
+            },
+          });
+        const posts = await Post.find({ userHandle: user.handle });
+        const comments = await Comment.find({ userHandle: user.handle });
 
         if (user) {
           const image = req.file.path;
           const result = await cloudinary.uploader.upload(image);
 
           user.profilePicture = result.url;
+
+          for (let i = 0; i < posts.length; i++) {
+            const currentPost = posts[i];
+
+            if (currentPost.userHandle === user.handle) {
+              currentPost.userImage = result.url;
+            }
+
+            await currentPost.save();
+          }
+
+          for (let i = 0; i < comments.length; i++) {
+            const currentComment = comments[i];
+
+            if (currentComment.userHandle === user.handle) {
+              currentComment.userImage = result.url;
+            }
+
+            await currentComment.save();
+          }
 
           await user.save();
 
@@ -72,21 +112,128 @@ userRouter.post(
 
 userRouter.put('/profile', getTokenFrom, async (req, res) => {
   try {
-    const userDetails = cleanUserDetails(req.body);
-    const { userId } = req.params;
+    const { errors, isValid } = validateProfile(req.body);
 
-    const user = await User.findById(userId);
-    const keysOfUserDetails = Object.keys(userDetails);
-
-    for (let i = 0; i < keysOfUserDetails.length; i++) {
-      const key = keysOfUserDetails[i];
-
-      user[key] = userDetails[key];
+    if (!isValid) {
+      return res.status(400).json(errors);
     }
 
-    await user.save();
+    const userDetails = cleanUserDetails(req.body);
+    const decodedToken = jwt.verify(req.token, config.JWT_SECRET);
 
-    res.status(200).json(user);
+    if (!req.token || !decodedToken.id) {
+      return res.status(401).json({
+        error: 'Unauthorized!',
+      });
+    } else {
+      const userId = decodedToken.id;
+      const user = await User.findById(userId);
+      const keysOfUserDetails = Object.keys(userDetails);
+
+      if (user) {
+        await user.populate('posts').populate('followers').execPopulate();
+
+        for (let i = 0; i < keysOfUserDetails.length; i++) {
+          const key = keysOfUserDetails[i];
+
+          user[key] = userDetails[key];
+        }
+
+        await user.save();
+
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({
+          error: 'User does not exist!',
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: 'Something went wrong!',
+    });
+  }
+});
+
+userRouter.post('/follow', getTokenFrom, async (req, res) => {
+  try {
+    const decodedToken = jwt.verify(req.token, config.JWT_SECRET);
+
+    if (!req.token || !decodedToken.id) {
+      return res.status(401).json({
+        error: 'Unauthorized!',
+      });
+    } else {
+      const { handle } = req.body;
+      const idOfUser = decodedToken.id;
+      const user = await User.findOne({ handle });
+      const follower = await Follower.create({ user: idOfUser });
+
+      if (user) {
+        user.followers.push(follower._id);
+
+        await user.save();
+        await user
+          .populate('posts')
+          .populate({
+            path: 'followers',
+            populate: {
+              path: 'user',
+              model: 'User',
+            },
+          })
+          .execPopulate();
+
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({
+          error: 'User does not exist!',
+        });
+      }
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: 'Something went wrong!',
+    });
+  }
+});
+
+userRouter.post('/unfollow', getTokenFrom, async (req, res) => {
+  try {
+    const decodedToken = jwt.verify(req.token, config.JWT_SECRET);
+
+    if (!req.token || !decodedToken.id) {
+      return res.status(401).json({
+        error: 'Unauthorized!',
+      });
+    } else {
+      const { handle } = req.body;
+      const idOfUser = decodedToken.id;
+      const userFollowing = await User.findById(idOfUser);
+      const user = await User.findOne({ handle })
+        .populate('posts')
+        .populate({
+          path: 'followers',
+          populate: {
+            path: 'user',
+            model: 'User',
+          },
+        });
+
+      if (user) {
+        user.followers = user.followers.filter(
+          (follower) => follower.user.handle !== userFollowing.handle
+        );
+
+        await user.save();
+
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({
+          error: 'User does not exist!',
+        });
+      }
+    }
   } catch (error) {
     res.status(500).json({
       error: 'Something went wrong!',
