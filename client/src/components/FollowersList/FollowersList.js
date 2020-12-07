a85739da33288088f45c1bb5bf5f7b310df7de59
Kristy Li <kristyli20.0@gmail.com>
2020-12-06 22:47:00 -0500

    feat: Create a FollowerList component to display the followers of a user

---
+++ client/src/components/FollowersList/FollowersList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import { useSelector } from 'react-redux';
+import Followers from './Followers/Followers';
+
+const styles = (theme) => ({
+  paper: {
+    padding: 20,
+    marginTop: 30,
+  },
+  title: {
+    textDecoration: 'underline',
+  },
+});
+
+const FollowersList = ({ classes }) => {
+  const user = useSelector((state) => state.data.user);
+  const { followers } = user;
+
+  return (
+    <Paper className={classes.paper}>
+      <Typography variant="h5" style={styles.title}>
+        Followers
+      </Typography>
+      <Followers followers={followers} />
+    </Paper>
+  );
+};
+
+export default withStyles(styles)(FollowersList);
+
+FollowersList.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
