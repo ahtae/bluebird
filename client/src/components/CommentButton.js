19369d5c135f8059b3dece6c0c45cfcfa2cb2a4c
Kristy Li <kristyli20.0@gmail.com>
2020-12-06 23:31:58 -0500

    feat: Create a CommentButton component

---
+++ client/src/components/CommentButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import ChatIcon from '@material-ui/icons/Chat';
+
+const CommentButton = () => {
+  return (
+    <Tooltip title="Show comments">
+      <IconButton color="primary">
+        <ChatIcon color="primary" />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default CommentButton;
