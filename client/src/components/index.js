e7bd42c53e7bf9d13e2b13503a6bfc806c811e86
Kristy Li <kristyli20.0@gmail.com>
2020-12-06 23:32:43 -0500

    chore: Import and export components

---
+++ client/src/components/index.js
@@ -0,0 +1,15 @@
+import NavBar from './NavBar';
+import PrivateRoute from './PrivateRoute';
+import Posts from './Posts/Posts';
+import Profile from './Profile';
+import PostForm from './PostForm';
+import FollowersList from './FollowersList/FollowersList';
+import Notifications from './Notifications/Notifications';
+
+export { NavBar };
+export { PrivateRoute };
+export { Posts };
+export { Profile };
+export { PostForm };
+export { FollowersList };
+export { Notifications };
