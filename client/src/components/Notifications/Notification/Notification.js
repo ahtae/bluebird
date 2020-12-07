9261a84f8af8965755e84ee0974cd2295533ce01
Kristy Li <kristyli20.0@gmail.com>
2020-12-07 10:16:03 -0500

    feat: Create a Notification component

---
+++ client/src/components/Notifications/Notification/Notification.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ChatIcon from '@material-ui/icons/Chat';
+import { useDispatch } from 'react-redux';
+import { readANotification } from '../../../redux/actions/data';
+import { useToasts } from 'react-toast-notifications';
+import { useSelector } from 'react-redux';
+import { clearErrors } from '../../../redux/actionCreators/ui';
+
+const styles = (theme) => ({
+  icon: { marginRight: 10 },
+  container: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'flex-start',
+    alignItems: 'space-between',
+    padding: '20px',
+  },
+  message: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    justifyContent: 'baseline',
+    alignItems: 'center',
+    width: '65%',
+    paddingRight: '20px',
+  },
+  isRead: { paddingLeft: '165px' },
+  isNotRead: { paddingLeft: '70px' },
+});
+
+const Notification = ({ classes, notification }) => {
+  const dispatch = useDispatch();
+  const verb = notification.message.includes('like')
+    ? 'like'
+    : notification.message.includes('made')
+    ? 'made'
+    : 'comment';
+  const time = dayjs(notification.createdAt).fromNow();
+  const iconColor = notification.isRead ? 'primary' : 'secondary';
+  const icon =
+    verb === 'like' ? (
+      <FavoriteIcon color={iconColor} className={classes.icon} />
+    ) : (
+      <ChatIcon color={iconColor} className={classes.icon} />
+    );
+  const ui = useSelector((state) => state.ui);
+  const { errors } = ui;
+  const { addToast } = useToasts();
+
+  const handleReadNotificationClick = (event) => {
+    event.preventDefault();
+
+    dispatch(readANotification(notification.id));
+
+    if (Object.keys(errors).length) {
+      addToast(`Unsuccessfully read comment! ${errors.error}`, {
+        appearance: 'error',
+        autoDismiss: true,
+      });
+
+      dispatch(clearErrors());
+    } else {
+      addToast('Successfully read comment!', {
+        appearance: 'success',
+        autoDismiss: true,
+      });
+    }
+  };
+
+  return (
+    <div className={classes.container}>
+      <Typography
+        component={Link}
+        color="default"
+        className={classes.message}
+        variant="body1"
+        to={`/posts/${notification.postId}`}
+      >
+        {icon}
+
+        {notification.message}
+      </Typography>
+      {!notification.isRead && (
+        <Typography
+          color="default"
+          variant="body1"
+          onClick={handleReadNotificationClick}
+        >
+          Mark as read
+        </Typography>
+      )}
+      <Typography
+        color="default"
+        variant="body2"
+        className={notification.isRead ? classes.isRead : classes.isNotRead}
+      >
+        {' '}
+        {time}
+      </Typography>
+    </div>
+  );
+};
+
+export default withStyles(styles)(Notification);
+
+Notification.propTypes = {
+  classes: PropTypes.object.isRequired,
+  notification: PropTypes.object.isRequired,
+};
