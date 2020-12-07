e7d7431de0024b2e3be2bd6ca40e6708bcfa0e75
Kristy Li <kristyli20.0@gmail.com>
2020-12-06 22:54:13 -0500

    feat: Create a Followers component

---
+++ client/src/components/FollowersList/Followers/Followers.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import Follower from './Follower/Follower';
+
+const styles = (theme) => ({
+  body: { paddingTop: 10 },
+  followersContainer: { display: 'flex', flexWrap: 'wrap' },
+});
+
+const Followers = ({ classes, followers }) => {
+  if (!followers.length) {
+    return (
+      <Typography variant="body2" className={classes.body}>
+        No followers yet!
+      </Typography>
+    );
+  }
+
+  return (
+    <div className={classes.followersContainer}>
+      {followers.map((follower) => (
+        <Follower key={follower.user.id} follower={follower.user} />
+      ))}
+    </div>
+  );
+};
+
+export default withStyles(styles)(Followers);
+
+Followers.propTypes = {
+  classes: PropTypes.object.isRequired,
+  followers: PropTypes.object.isRequired,
+};
