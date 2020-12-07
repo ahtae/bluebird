cdff0e37aabba94724dfebbf74151b158119fbd4
Kristy Li <kristyli20.0@gmail.com>
2020-12-06 22:40:39 -0500

    chore: Require and export controllers

---
+++ server/controllers/index.js
@@ -0,0 +1,11 @@
+const postRouter = require('./post');
+const authRouter = require('./auth');
+const userRouter = require('./user');
+const notificationRouter = require('./notification');
+
+module.exports = {
+  postRouter,
+  authRouter,
+  userRouter,
+  notificationRouter,
+};
