f6d5f7ab4cf23bb2579c4565d846dc0358e533ff
Kristy Li <kristyli20.0@gmail.com>
2020-12-27 21:38:20 -0500

    fix: Export  NODE_ENV variable

--- server/utils/config.js
+++ server/utils/config.js
@@ -3,7 +3,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  PORT: process.env.PORT || 8000,
+  PORT: process.env.PORT,
   MONGODB_URI: process.env.MONGODB_URI,
   JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
 };
